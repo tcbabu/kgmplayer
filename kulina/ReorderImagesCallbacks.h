7631783ee7df61df86f4b92507641b080a708b94
Thomas C Babu <chackuva@gmail.com>
2023-01-22 15:49:12 +0530

    Reorder added

---
+++ kulina/ReorderImagesCallbacks.h
@@ -0,0 +1,10 @@
+int  ReorderImagessplbutton1callback(int butno,int i,void *Tmp) ;
+void  ReorderImagessplbutton1init(DIL *B,void *pt) ;
+int  ReorderImagesbrowser1callback(int item,int i,void *Tmp) ;
+void  ReorderImagesbrowser1init(DIY *Y,void *pt) ;
+int ReorderImagesinit(void *Tmp) ;
+int ReorderImagescleanup(void *Tmp) ;
+int ModifyReorderImages(void *Tmp,int GrpId) ;
+int ReorderImagesCallBack(void *Tmp,void *tmp) ;
+int ReorderImagesResizeCallBack(void *Tmp) ;
+int ReorderImagesWaitCallBack(void *Tmp) ;
