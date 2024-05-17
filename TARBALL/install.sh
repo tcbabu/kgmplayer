0186851b902a44065e8013976e2ca1eed382d0cc
Kulina <kulinasys@gmail.com>
2024-05-17 20:44:07 +0530

    tarball updated

--- TARBALL/install.sh
+++ TARBALL/install.sh
@@ -5,9 +5,12 @@ ln -sf /usr/bin/kgmplayer /usr/bin/kglame
 ln -sf /usr/bin/kgmplayer /usr/bin/kgffmpeg
 ln -sf /usr/bin/kgmplayer /usr/bin/kgmencoder
 mkdir -p /usr/share/kgmplayer
+mkdir -p /usr/share/kgmplayer/lib
 install -m 644 kgmplayer.png /usr/share/kgmplayer
 install -m 644 empty.srt /usr/share/kgmplayer
 install -m 644 empty.idx /usr/share/kgmplayer
 install -m 644 empty.sub /usr/share/kgmplayer
 install -m 644 kgmplayer.desktop /usr/share/applications
 install -m 644 kgMplayer.desktop /usr/share/applications
+cp  lib/* /usr/share/kgmplayer/lib/
+
